9799b23e91d4e4da1132dbbb1f6554add960b114
Sven Hettwer <sven.hettwer@consol.de>
2019-10-10 15:05:44 +0200

    Added Sakuli package.json and updated test cases for Sakuli 2

--- e2e/layout/testLayout/layout_test.js
+++ e2e/layout/testLayout/layout_test.js
@@ -1,21 +1,24 @@
-_dynamicInclude($includeFolder);
-var testCase = new TestCase(50, 70);
+(async () => {
+    var testCase = new TestCase();
 
-try {
+    try {
+        await _navigateTo("http://todo-app-int.192.168.99.108.nip.io/todolist");
+        //Check laylout
+        await _highlight(_heading1("TODO list"));
+        await _highlight(_list("list-group"));
+        await _highlight(_heading2("New TODO entry"));
+        await _highlight(_label("Title"));
+        await _highlight(_textbox("title"));
+        await _highlight(_label("Description"));
+        await _highlight(_textarea("description"));
+        await _highlight(_submit("Add"));
+        await testCase.endOfStep("Test todo page layout", 30);
 
-    //Check laylout
-    _highlight(_heading1("TODO list"));
-    _highlight(_list("list-group"));
-    _highlight(_heading2("New TODO entry"));
-    _highlight(_label("Title"));
-    _highlight(_textbox("title"));
-    _highlight(_label("Description"));
-    _highlight(_textarea("description"));
-    _highlight(_submit("Add"));
-    testCase.endOfStep("Test todo page layout", 30);
+    } catch (e) {
+        testCase.handleException(e);
+    } finally {
+        testCase.saveResult();
+    }
+})().then(done);
 
-} catch (e) {
-    testCase.handleException(e);
-} finally {
-    testCase.saveResult();
-}
+
--- e2e/lifeCycle/testTodoLifecycle/lifecycle_test.js
+++ e2e/lifeCycle/testTodoLifecycle/lifecycle_test.js
@@ -1,51 +1,52 @@
-_dynamicInclude($includeFolder);
-var testCase = new TestCase(50, 70);
-var env = new Environment();
-var screen = new Region();
+(async () => {
+    var testCase = new TestCase();
+    var env = new Environment();
+    var screen = new Region();
 
-var pdfFilePath = "~/todo-backup.pdf";
-try {
+    var pdfFilePath = "~/todo-backup.pdf";
+    try {
 
-    // Add entry
-    _highlight(_textbox("title"));
-    _click(_textbox("title"));
-    _setValue(_textbox("title"), "Sample todo");
+        // Add entry
+        await _highlight(_textbox("title"));
+        await _click(_textbox("title"));
+        await _setValue(_textbox("title"), "Sample todo");
 
-    _highlight(_textarea("description"));
-    _click(_textarea("description"));
-    _setValue(_textarea("description"), "Sample todo description");
+        await _highlight(_textarea("description"));
+        await _click(_textarea("description"));
+        await _setValue(_textarea("description"), "Sample todo description");
 
-    _highlight(_submit("Add"));
-    _click(_submit("Add"));
-    testCase.endOfStep("Add todo entry", 30);
+        await _highlight(_submit("Add"));
+        await _click(_submit("Add"));
+        await testCase.endOfStep("Add todo entry", 30);
 
-    //load images
-    testCase.addImagePaths("centos_chrome");
+        //load images
+        await testCase.addImagePaths("centos_chrome");
 
-    //open print preview
-    env.type("p", Key.CTRL);
+        //open print preview
+        await env.type("p", Key.CTRL);
 
-    //save as pdf
-    screen.find("save_button").highlight().click();
-    env.sleep(1);
-    env.type("a", Key.CTRL) //mark filename in "save under" dialog
-        .type(pdfFilePath + Key.ENTER) //type filename and press ENTER
-        .sleep(1);
-    testCase.endOfStep("Create backup of todo entry", 30);
+        //save as pdf
+        await screen.find("save_button").highlight().click();
+        await env.sleep(1);
+        await env.type("a", Key.CTRL) //mark filename in "save under" dialog
+            .type(pdfFilePath + Key.ENTER) //type filename and press ENTER
+            .sleep(1);
+        await testCase.endOfStep("Create backup of todo entry", 30);
 
-    //Complete entry
-    _highlight(_listItem("Sample todox"));
-    _highlight(_checkbox("complete"));
-    _click(_checkbox("complete"));
-    testCase.endOfStep("Complete todo entry", 30);
+        //Complete entry
+        await _highlight(_listItem("Sample todox"));
+        await _highlight(_checkbox("complete"));
+        await _click(_checkbox("complete"));
+        await testCase.endOfStep("Complete todo entry", 30);
 
-    //Delete entry
-    _highlight(_span("x"));
-    _click(_span("x"));
-    testCase.endOfStep("Delete todo entry", 30);
+        //Delete entry
+        await _highlight(_span("x"));
+        await _click(_span("x"));
+        await testCase.endOfStep("Delete todo entry", 30);
 
-} catch (e) {
-    testCase.handleException(e);
-} finally {
-    testCase.saveResult();
-}
+    } catch (e) {
+        testCase.handleException(e);
+    } finally {
+        testCase.saveResult();
+    }
+})().then(done);
