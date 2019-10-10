ab9f3334dd8a90151fbfad79f93d80c42a6d4f4a
Sven Hettwer <sven.hettwer@consol.de>
2019-10-10 12:03:22 +0200

    Moved GOGS to user namespace

--- paas/bootstrap.sh
+++ paas/bootstrap.sh
@@ -18,13 +18,14 @@ minishift start \
 
 oc login -u system:admin
 
+#Setup offline s2i build
+oc project openshift
+oc process -f $(git rev-parse --show-toplevel)/paas/yml/s2i_spring_offline_build_template.yml | oc apply -f -
+oc start-build -w spring-offline-s2i
+
+oc login -u developer -p whateverYourPasswordWouldBe
 #Setup gogs repository
 oc new-project gogs
 oc new-app -f http://bit.ly/openshift-gogs-template --param=HOSTNAME=gogs.$(minishift ip).nip.io
 #First registered user is admin
 #Manual import of the project into gogs required https://github.com/svettwer/JCON-2018-Containerized-End-2-End-Testing.git as 'todo-app'
-
-#Setup offline s2i build
-oc project openshift
-oc process -f $(git rev-parse --show-toplevel)/paas/yml/s2i_spring_offline_build_template.yml | oc apply -f -
-oc start-build -w spring-offline-s2i
