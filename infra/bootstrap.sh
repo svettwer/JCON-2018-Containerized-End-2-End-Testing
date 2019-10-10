58f1349cd70050fe486d173dab71987c55fc2efe
Sven Hettwer <sven.hettwer@consol.de>
2019-10-10 11:58:41 +0200

    Updated infra boostrap.sh

--- infra/bootstrap.sh
+++ infra/bootstrap.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+function help() {
+    echo "Usage: sh bootstrap.sh <DOCKER_USERNAME> <DOCKER_PASSWORD>"
+    echo ""
+    echo "Parameters:"
+    echo "  DOCKER_USERNAME: Dockerhub user with access to taconsol"
+    echo "  DOCKER_PASSWORD: Password of the dockerhub user"
+}
+
+DOCKER_USERNAME="${1}"
+DOCKER_PASSWORD="${2}"
+
+[[ -z "${DOCKER_USERNAME}" ]] && echo "ERROR: DOCKER_USERNAME is empty" && help  && exit
+[[ -z "${DOCKER_PASSWORD}" ]] && echo "ERROR: DOCKER_PASSWORD is empty" && help && exit
+
 oc login -u developer -p whateverYourPasswordWouldBe
 
 oc delete project myproject
@@ -9,7 +23,20 @@ oc new-project todo-app-int
 oc new-project todo-app-prod
 
 oc project todo-app-dev
-oc process -f infra/yml/jenkins_template.yml | oc apply -f -
+oc process -f $(git rev-parse --show-toplevel)/infra/yml/jenkins_template.yml | oc apply -f -
+
+oc project todo-app-int
+oc create secret docker-registry dockerhub-sakuli-secret \
+    --docker-server=docker.io \
+    --docker-username=${DOCKER_USERNAME} \
+    --docker-password=${DOCKER_PASSWORD} \
+    --docker-email=unused
+oc secrets link builder dockerhub-sakuli-secret
+oc import-image sakuli-s2i \
+    --from=docker.io/taconsol/sakuli-s2i \
+    --confirm \
+    --scheduled=true \
+    --all=true
 
 oc policy add-role-to-user edit system:serviceaccount:todo-app-dev:jenkins -n todo-app-int
 oc policy add-role-to-user edit system:serviceaccount:todo-app-dev:jenkins -n todo-app-prod
